2cbd5d134419f867d705c543c5ce7a2ccb13b84f
Adrian Talonia <adrian.talonia11@gmail.com>
2020-12-04 14:09:16 -0600

    first commit

---
+++ src/main/java/com/atr/repository/TutorialRepository.java
@@ -0,0 +1,15 @@
+package com.atr.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.atr.models.Tutorial;
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+
+	List<Tutorial> findByTitleContaining(String title);
+
+	List<Tutorial> findByPublished(boolean published);
+
+}
---
+++ src/test/java/com/atr/CrudMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
