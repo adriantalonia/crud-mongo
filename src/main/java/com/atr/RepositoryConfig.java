993f82ac8d67eb73b164c777e29fe723274ad941
Adrian Talonia <adrian.talonia11@gmail.com>
2020-12-10 06:43:39 -0600

    update

---
+++ src/main/java/com/atr/RepositoryConfig.java
@@ -0,0 +1,16 @@
+package com.atr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.atr.models.Tutorial;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Tutorial.class);
+	}
+}
--- src/main/java/com/atr/repository/TutorialRepository.java
+++ src/main/java/com/atr/repository/TutorialRepository.java
@@ -3,13 +3,20 @@ package com.atr.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.atr.models.Tutorial;
 
+@RepositoryRestResource(path = "tutorials")
 public interface TutorialRepository extends MongoRepository<Tutorial, String> {
 
-	List<Tutorial> findByTitleContaining(String title);
+	@RestResource(path= "find-title")
+	List<Tutorial> findByTitleContaining(@Param("title")String title);
 
 	List<Tutorial> findByPublished(boolean published);
+	
+
 
 }
